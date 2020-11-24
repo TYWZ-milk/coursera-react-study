682e3ef1c999b7e1674362466512ccaf23eb1919
yitongwang <tongyiwangzaimilk@gmail.com>
2020-11-24 11:25:31 -0600

    React Router

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,7 @@ import {Navbar, NavbarBrand} from 'reactstrap';
 import React, {Component} from 'react';
 import Main from "./components/MainComponent";
 import './App.css';
+import {BrowserRouter} from "react-router-dom";
 
 class App extends Component{
 
@@ -9,9 +10,11 @@ class App extends Component{
   render()
   {
     return (
+        <BrowserRouter>
         <div>
           <Main/>
         </div>
+        </BrowserRouter>
     );
   }
 }
